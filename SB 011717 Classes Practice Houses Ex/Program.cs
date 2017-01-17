64a04b35e989df88aa84fd4af1c4711821940bf3
Satnam Brar <satnamsbrar@gmail.com>
2017-01-17 11:56:06 -0500

    first commit

---
+++ SB 011717 Classes Practice Houses Ex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB_011717_Classes_Practice_Houses_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
