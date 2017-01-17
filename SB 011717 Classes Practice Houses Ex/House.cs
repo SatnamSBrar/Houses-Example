5865cb7b3ca9a8a71a1ca967b6dc360d1768d899
Satnam Brar <satnamsbrar@gmail.com>
2017-01-17 14:28:59 -0500

    added two methods

--- SB 011717 Classes Practice Houses Ex/House.cs
+++ SB 011717 Classes Practice Houses Ex/House.cs
@@ -8,5 +8,19 @@ namespace SB_011717_Classes_Practice_Houses_Ex
 {
     class House
     {
+        private string color;               //set to private so it cannot be modified
+        private int rooms;
+        private int size;
+        private int doors;
+
+        public void Paint (string color)    //set to public so it can be used publicly
+        {
+            this.color = color;             //this. refers to class House
+                                            //sets field color to the string parameter input via the Paint method
+        }
+        public string CurrentColor ()
+        {
+            return color;
+        }
     }
 }
