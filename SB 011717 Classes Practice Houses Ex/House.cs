f9f584fd68624d5d09626bee5a40b412813ebb63
Satnam Brar <satnamsbrar@gmail.com>
2017-01-18 10:18:03 -0500

    added constructor and display method

---
+++ SB 011717 Classes Practice Houses Ex/House.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB_011717_Classes_Practice_Houses_Ex
+{
+    class House
+    {
+        private string color;               //set to private so it cannot be modified
+        private int rooms;
+        private int size;
+        private int doors;
+
+        public House(string color, int rooms, int size, int doors)  //set parameters for class House
+        {
+            this.color = color;
+            this.rooms = rooms;
+            this.size = size;
+            this.doors = doors;
+        }
+
+        public void Paint(string color)    //set to public so it can be used publicly
+        {
+            this.color = color;             //this. refers to class House
+                                            //sets field color to the string parameter input via the Paint method
+        }
+        public string CurrentColor()
+        {
+            return color;
+        }
+
+        public void DisplayHouseInfo()  //method to display House information
+        {
+            Console.WriteLine("\nHouse Color: " + color);
+            Console.WriteLine("Number of Rooms: " + rooms);
+            Console.WriteLine("Number of Doors: " + doors);
+            Console.WriteLine("Square Feet: " + size + "\n");
+        }
+    }
+}
--- SB 011717 Classes Practice Houses Ex/Program.cs
+++ SB 011717 Classes Practice Houses Ex/Program.cs
@@ -6,27 +6,11 @@ using System.Threading.Tasks;
 
 namespace SB_011717_Classes_Practice_Houses_Ex
 {
-    class House
-    {
-        private string color;               //set to private so it cannot be modified
-        private int rooms;
-        private int size;
-        private int doors;
-
-        public void Paint(string color)    //set to public so it can be used publicly
-        {
-            this.color = color;             //this. refers to class House
-                                            //sets field color to the string parameter input via the Paint method
-        }
-        public string CurrentColor()
-        {
-            return color;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
+            /*
             House house1 = new House(); //creates new object in class House
 
             house1.Paint("red");        //paints house1 red
@@ -40,6 +24,10 @@ namespace SB_011717_Classes_Practice_Houses_Ex
             house2.Paint("yellow");     //paints house2 yellow
 
             Console.WriteLine("\nHouse 1 is " + house1.CurrentColor() + ",and House 2 is " + house2.CurrentColor() + ".\n");
+            */
+
+            House house1 = new House("White", 15, 10000, 7);
+            house1.DisplayHouseInfo();
         }
     }
 }
